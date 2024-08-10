5b50372ac18de26d7605aa2d54f77696888b3b3c
GALLIE <professional.gallie@gmail.com>
2024-08-10 18:40:58 +0000

    Creation of a fonction for to redirect to shop.html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const activePage = window.location.pathname;
 const navLinks1 = document.querySelectorAll('header a');
+const btn = document.querySelector('.btn')
 
 navLinks1.forEach(link => {
     if(link.href.includes(`${activePage}`)) {
         link.classList.add('active')
     }
+})
+
+btn.addEventListener('click', () => {
+    window.location.href= 'shop.html';
 })
  
 
