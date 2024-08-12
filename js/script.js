5c2e76c30b38cf7a7aa690ec9eeb38629d349c99
GALLIE <professional.gallie@gmail.com>
2024-08-12 18:43:59 +0000

    Custom scroll bar

--- js/script.js
+++ js/script.js
@@ -6,10 +6,16 @@ navLinks1.forEach(link => {
     if(link.href.includes(`${activePage}`)) {
         link.classList.add('active')
     }
-})
+});
 
-btn_shop.addEventListener('click', () => {
-    window.location.href= 'shop.html';
-})
- 
 
+
+// Custom Scroll bar 
+window.onscroll = () => {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.querySelector('#scroll-bar').style.width= scrolled + '%';
+};
+
+console.log(document.querySelector('#scroll-bar'))
